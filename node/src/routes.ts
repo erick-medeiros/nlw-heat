3f19b5ebe6837369381a2ccf5d6020d7fd988651
Erick Medeiros <erickmedeirosbr@gmail.com>
2021-11-03 18:57:59 -0300

    first commit

---
+++ node/src/routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
+import { ensureAuthenticate } from "./middleware/ensureAuthenticate";
+
+const routes = Router()
+
+routes.post('/authenticate',new AuthenticateUserController().handle)
+
+routes.post('/messages', ensureAuthenticate, new CreateMessageController().handle)
+
+routes.get('/messages/last3', new GetLast3MessagesController().handle)
+
+routes.get('/profile', ensureAuthenticate, new ProfileUserController().handle)
+
+export { routes }
